8efc959e1fccec29624220fd584dfdccf055cd66
avi bansal <avi19985@gmail.com>
2026-01-10 19:45:27 +0530

    Deployment

--- app/api/auth/google/route.js
+++ app/api/auth/google/route.js
@@ -5,6 +5,7 @@ const redirectUri = process.env.NEXT_PUBLIC_BASE_URL
     ? `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback/google`
     : "http://localhost:3000/api/auth/callback/google";
 
+// test
 const client = new OAuth2Client(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
